d19fbad69745742b9b5c7c18efd9df562ba9c38c
Dillon Plunkett <dillonplunkett@gmail.com>
2020-03-16 22:56:27 -0400

    handle split cards (and other odd characters in card names)

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask_bootstrap import Bootstrap, WebCDN
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
+from app import filters
 from config import Config
 
 
@@ -18,6 +19,12 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 socketio = SocketIO(app)
+
+@app.template_filter()
+def url_for_card_name(card_name):
+    crd = "".join(x for x in card_name if x.isalnum())
+    return url_for("static", filename="card_images/"+ crd + ".jpg")
+
 
 from app import routes, models
 
--- fetch_images.py
+++ fetch_images.py
@@ -1,22 +1,22 @@
 import os
 import time
 from urllib.request import urlopen, urlretrieve
-from urllib.parse import urlencode
+from urllib.parse import quote
 
 imgs_dir = "app/static/card_images"
 
 with open("cube_list.txt", 'r') as cube_file:
-    cube_list = [line.strip().split(" //")[0] for line in cube_file]  # TODO: Do split cards works?
+    cube_list = [line.strip() for line in cube_file]
 
 # TODO: Use bulk data from Scryfall to make this faster.
 
 os.makedirs(imgs_dir, exist_ok=True)
 
 for card in cube_list:
-    image_file = os.path.join(imgs_dir, f"{card}.jpg".replace(" ", "_"))
+    image_file = os.path.join(imgs_dir, f"{''.join(x for x in card if x.isalnum())}.jpg".replace(" ", "_"))
     if not os.path.isfile(image_file):
         print(card)
-        redir_to_image = f"https://api.scryfall.com/cards/named?fuzzy={card}&format=image&version=normal".replace(" ", "%20") # TODO: 404?
+        redir_to_image = f"https://api.scryfall.com/cards/named?fuzzy={quote(card)}&format=image&version=normal" # TODO: 404?
         image_url = urlopen(redir_to_image).geturl()
         urlretrieve(redir_to_image, image_file)
         # Scryfall requests waiting 100ms between requests.
