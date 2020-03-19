adae586ced0fab6d03156186ac8790b4925ded6e
Dillon Plunkett <dillonplunkett@gmail.com>
2020-03-18 23:31:36 -0400

    make possible to run without development server

--- auction_draft.py
+++ auction_draft.py
@@ -1,7 +1,11 @@
-from app import app, db
+from app import app, db, socketio
 from app.models import User, Bid, Balance
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {"db": db, "User": User, "Bid": Bid, "Balance": Balance}
+
+
+if __name__ == "__main__":
+    socketio.run(app)
